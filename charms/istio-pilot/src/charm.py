05f6502406b768f06aa46fe2d8ee2d6a01d2ff7c
Andrew Scribner <ca.scribner+1@gmail.com>
2022-01-06 16:55:54 -0500

    fix ingress-auth add tests, and other changes
    Fixes the ingress-auth relation, which used CRDs that were removed from this version of Istio. Unit tests pass, but not yet fully integration tested with dex/oidc-gatekeeper
    
    Incorporates the following changes from johnsca's [outstanding PR](https://github.com/canonical/istio-operators/pull/43):
    - Splits out default_gateway handling from ingress handling.  Adds/refactors tests for this
    - adds destination rule creation and refactors remove hook
    
    Fixes bug when using multiple ingress relation where the template rendering merged the yaml for the different virtualservices together
    
    Fixes possible bug where, previously, when handling relation changes we deleted all VirtualService, EnvoyFilters, etc. that were created by this juju app, and then recreated the objects we wanted. This had the unintended consequence of removing any objects that istiod (which is also deployed by this application) created. This fix adds an extra label to identify istio entities created by this charm's workload from those created by other sources within the application
    
    (from knkski) Fixes case where charm fails if ingress auth routes list is empty.  Previously,
    `manifests` would never be empty, due to `rbac_configs`. With that removal, `manifests` might be empty. In which case, we skip creating it.

--- charms/istio-pilot/src/charm.py
+++ charms/istio-pilot/src/charm.py
@@ -2,11 +2,10 @@
 
 import logging
 import subprocess
-from pathlib import Path
 
 import yaml
 from jinja2 import Environment, FileSystemLoader
-from ops.charm import CharmBase
+from ops.charm import CharmBase, RelationBrokenEvent
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
 from serialized_data_interface import NoCompatibleVersions, NoVersionsListed, get_interfaces
@@ -39,10 +38,13 @@ class Operator(CharmBase):
         self.framework.observe(self.on.install, self.install)
         self.framework.observe(self.on.remove, self.remove)
 
+        self.framework.observe(self.on.config_changed, self.handle_default_gateways)
+
         self.framework.observe(self.on["istio-pilot"].relation_changed, self.send_info)
 
         self.framework.observe(self.on['ingress'].relation_changed, self.handle_ingress)
         self.framework.observe(self.on['ingress'].relation_departed, self.handle_ingress)
+        self.framework.observe(self.on['ingress'].relation_broken, self.handle_ingress)
         self.framework.observe(self.on['ingress-auth'].relation_changed, self.handle_ingress_auth)
         self.framework.observe(self.on['ingress-auth'].relation_departed, self.handle_ingress_auth)
 
@@ -78,29 +80,67 @@ class Operator(CharmBase):
             ]
         )
 
-        subprocess.run(
-            ["./kubectl", "delete", "-f-", "--ignore-not-found"],
-            input=manifests,
-            # Can't remove stuff yet: https://bugs.launchpad.net/juju/+bug/1941655
-            # check=True,
+        try:
+            self._kubectl(
+                "delete",
+                "virtualservices,destinationrule,gateways,envoyfilters",
+                f"-lapp.juju.is/created-by={self.app.name}"
+                f",app.{self.app.name}.io/is-workload-entity=true",
+                capture_output=True,
+            )
+            self._kubectl(
+                'delete',
+                "--ignore-not-found",
+                "-f-",
+                input=manifests,
+                capture_output=True,
+            )
+        except subprocess.CalledProcessError as e:
+            if "(Unauthorized)" in e.stderr.decode("utf-8"):
+                # Ignore error from https://bugs.launchpad.net/juju/+bug/1941655
+                pass
+            else:
+                self.log.error(e.stderr)
+                raise
+
+    def handle_default_gateways(self, event):
+        t = self.env.get_template('gateway.yaml.j2')
+        gateways = self.model.config['default-gateways'].split(',')
+        manifest = ''.join(t.render(name=g, app_name=self.app.name) for g in gateways)
+        self._kubectl(
+            'delete',
+            'gateways',
+            f"-lapp.juju.is/created-by={self.app.name},"
+            f"app.{self.app.name}.io/is-workload-entity=true",
         )
+        self._kubectl("apply", "-f-", input=manifest)
 
     def send_info(self, event):
         if self.interfaces["istio-pilot"]:
             self.interfaces["istio-pilot"].send_data(
-                {
-                    "service-name": f'istiod.{self.model.name}.svc',
-                    "service-port": '15012',
-                }
+                {"service-name": f'istiod.{self.model.name}.svc', "service-port": '15012'}
             )
 
     def handle_ingress(self, event):
+        gateway_address = self._get_gateway_address()
+        if not gateway_address:
+            self.unit.status = WaitingStatus("Waiting for gateway address")
+            event.defer()
+            return
+        else:
+            self.unit.status = ActiveStatus()
+
         ingress = self.interfaces['ingress']
 
         if ingress:
             routes = ingress.get_data().items()
         else:
-            routes = []
+            routes = {}
+
+        if isinstance(event, RelationBrokenEvent):
+            # The app-level data is still visible on a broken relation, but we
+            # shouldn't be keeping the VirtualService for that related app.
+            del routes[(event.relation, event.app)]
 
         t = self.env.get_template('virtual_service.yaml.j2')
         gateway = self.model.config['default-gateways'].split(',')[0]
@@ -110,41 +150,26 @@ class Operator(CharmBase):
 
             v1 ingress schema doesn't allow sending over a namespace.
             """
-            kwargs = {'gateway': gateway, **route}
+            kwargs = {'gateway': gateway, 'app_name': self.app.name, **route}
 
             if 'namespace' not in kwargs:
                 kwargs['namespace'] = self.model.name
 
             return kwargs
 
-        virtual_services = ''.join(
-            t.render(**get_kwargs(ingress.versions[app.name], route))
+        virtual_services = '\n---'.join(
+            t.render(**get_kwargs(ingress.versions[app.name], route)).strip().strip("---")
             for ((_, app), route) in routes
         )
 
-        t = self.env.get_template('gateway.yaml.j2')
-        gateways = self.model.config['default-gateways'].split(',')
-        gateways = ''.join(t.render(name=g) for g in gateways)
-
-        manifests = [virtual_services, gateways]
-        manifests = '\n'.join([m for m in manifests if m])
-
-        subprocess.run(
-            [
-                './kubectl',
-                'delete',
-                'virtualservices,gateways',
-                f'-lapp.juju.is/created-by={self.model.app.name}',
-                '-n',
-                self.model.name,
-            ],
-            check=True,
+        self._kubectl(
+            'delete',
+            'virtualservices,destinationrules',
+            f"-lapp.juju.is/created-by={self.app.name}"
+            f",app.{self.app.name}.io/is-workload-entity=true",
         )
-        subprocess.run(
-            ["./kubectl", "apply", "-f-"],
-            input=manifests.encode('utf-8'),
-            check=True,
-        )
+        if routes:
+            self._kubectl("apply", "-f-", input=virtual_services)
 
     def handle_ingress_auth(self, event):
         auth_routes = self.interfaces['ingress-auth']
@@ -153,16 +178,21 @@ class Operator(CharmBase):
         else:
             auth_routes = []
 
-        if not all(ar.get("service") for ar in auth_routes):
-            self.model.unit.status = WaitingStatus("Waiting for auth route connection information.")
+        if not auth_routes:
+            self.log.info("Skipping auth route creation due to empty list")
             return
 
-        rbac_configs = Path('src/rbac_config.yaml').read_text() if auth_routes else None
+        if not all(ar.get("service") for ar in auth_routes):
+            self.model.unit.status = WaitingStatus(
+                "Waiting for auth route connection information."
+            )
+            return
 
         t = self.env.get_template('auth_filter.yaml.j2')
         auth_filters = ''.join(
             t.render(
                 namespace=self.model.name,
+                app_name=self.app.name,
                 **{
                     'request_headers': yaml.safe_dump(
                         [{'exact': h} for h in r.get('allowed-request-headers', [])],
@@ -179,25 +209,57 @@ class Operator(CharmBase):
             for r in auth_routes
         )
 
-        manifests = [rbac_configs, auth_filters]
+        manifests = [auth_filters]
         manifests = '\n'.join([m for m in manifests if m])
-        subprocess.run(
-            [
-                './kubectl',
-                'delete',
-                'envoyfilters,rbacconfigs',
-                f'-lapp.juju.is/created-by={self.model.app.name}',
-                '-n',
-                self.model.name,
-            ],
-            check=True,
+        self._kubectl(
+            'delete',
+            'envoyfilters',
+            f"-lapp.juju.is/created-by={self.app.name},app.istio-pilot.io/is-workload-entity=true",
         )
 
-        subprocess.run(
-            ["./kubectl", "apply", "-f-"],
-            input=manifests.encode('utf-8'),
+        self._kubectl("apply", "-f-", input=manifests)
+
+    def _kubectl(self, *args, namespace=None, input=None, capture_output=False):
+        """Helper for running kubectl."""
+        if isinstance(input, str):
+            input = input.encode("utf-8")
+        res = subprocess.run(
+            [
+                './kubectl',
+                '-n',
+                namespace or self.model.name,
+                *args,
+            ],
+            input=input,
+            capture_output=capture_output,
             check=True,
         )
+        if capture_output:
+            return res.stdout.decode("utf-8")
+
+    def _get_gateway_address(self):
+        """Look up the load balancer address for the ingress gateway.
+
+        If the gateway isn't available or doesn't have a load balancer address yet,
+        returns None.
+        """
+        svcs = yaml.safe_load(
+            self._kubectl(
+                "get",
+                "svc",
+                "-l",
+                "istio=ingressgateway",
+                "-oyaml",
+                namespace=self.model.name,
+                capture_output=True,
+            )
+        )
+        if not svcs["items"]:
+            return None
+        addrs = svcs["items"][0]["status"].get("loadBalancer", {}).get("ingress", [])
+        if not addrs:
+            return None
+        return addrs[0].get("hostname", addrs[0].get("ip"))
 
 
 if __name__ == "__main__":
--- charms/istio-pilot/tests/test_charm.py
+++ charms/istio-pilot/tests/test_charm.py
@@ -7,6 +7,21 @@ from ops.model import ActiveStatus, WaitingStatus
 from ops.testing import Harness
 
 
+# autouse to ensure we don't accidentally call out, but
+# can also be used explicitly to get access to the mock.
+@pytest.fixture(autouse=True)
+def subprocess(mocker):
+    subprocess = mocker.patch("charm.subprocess")
+    for method_name in ("run", "call", "check_call", "check_output"):
+        method = getattr(subprocess, method_name)
+        method.return_value.returncode = 0
+        method.return_value.stdout = b""
+        method.return_value.stderr = b""
+        method.return_value.output = b""
+        mocker.patch(f"subprocess.{method_name}", method)
+    yield subprocess
+
+
 @pytest.fixture
 def harness():
     return Harness(Operator)
@@ -17,8 +32,8 @@ def test_not_leader(harness):
     assert harness.charm.model.unit.status == WaitingStatus('Waiting for leadership')
 
 
-def test_basic(harness, mocker):
-    check_call = mocker.patch('subprocess.check_call')
+def test_basic(harness, subprocess):
+    check_call = subprocess.check_call
     harness.set_leader(True)
     harness.begin_with_initial_hooks()
     container = harness.model.unit.get_container('noop')
@@ -41,18 +56,83 @@ def test_basic(harness, mocker):
     assert harness.charm.model.unit.status == ActiveStatus('')
 
 
-def test_with_ingress_relation(harness, mocker):
-    run = mocker.patch('subprocess.run')
-    check_call = mocker.patch('subprocess.check_call')
+def test_default_gateways(harness, subprocess):
+    run = subprocess.run
+    check_call = subprocess.check_call
 
     harness.set_leader(True)
     harness.add_oci_resource(
         "noop",
+        {"registrypath": "", "username": "", "password": ""},
+    )
+    harness.begin_with_initial_hooks()
+
+    run.reset_mock()
+    check_call.reset_mock()
+
+    default_gateways = "myGateway"
+    harness.update_config({"default-gateways": default_gateways})
+    gateway_names = default_gateways.split(',')
+
+    # Check deletion calls
+
+    expected_delete_call = Call([
+            './kubectl',
+            '-n',
+            None,
+            'delete',
+            'gateways',
+            '-lapp.juju.is/created-by=istio-pilot,app.istio-pilot.io/is-workload-entity=true',
+        ],
+        input=None,
+        capture_output=False,
+        check=True
+    )
+    assert expected_delete_call == run.call_args_list[0]
+
+    # Check creation calls
+
+    expected_manifest = [
         {
-            "registrypath": "",
-            "username": "",
-            "password": "",
-        },
+            'apiVersion': 'networking.istio.io/v1beta1',
+            'kind': 'Gateway',
+            'metadata': {
+                'name': this_gateway_name,
+                'labels': {'app.istio-pilot.io/is-workload-entity': "true"},
+            },
+            'spec': {
+                'selector': {'istio': 'ingressgateway'},
+                'servers': [
+                    {'hosts': ['*'], 'port': {'name': 'http', 'number': 80, 'protocol': 'HTTP'}}
+                ],
+            },
+        } for this_gateway_name in gateway_names
+    ]
+
+    actual_manifest = list(yaml.safe_load_all(run.call_args_list[1].kwargs['input'].decode("utf-8")))
+    assert actual_manifest == expected_manifest
+
+    expected_create_call_args = (
+        [
+            './kubectl',
+            '-n',
+            None,
+            "apply",
+            "-f-",
+        ],
+    )
+
+    assert run.call_args_list[1].args == expected_create_call_args
+
+
+def test_with_ingress_relation(harness, subprocess):
+    run = subprocess.run
+    check_call = subprocess.check_call
+
+    harness.set_leader(True)
+    harness.add_oci_resource(
+        "noop",
+        {"registrypath": "", "username": "", "password": ""},
     )
     rel_id = harness.add_relation("ingress", "app")
 
@@ -63,13 +143,28 @@ def test_with_ingress_relation(harness, mocker):
         "app",
         {"_supported_versions": "- v1", "data": yaml.dump(data)},
     )
+
+    # To mock `kubectl get svc` call's return of no objs
+    run.return_value.stdout = b"{'items': []}"
     harness.begin_with_initial_hooks()
+
+    assert run.call_count == 4
+    run.reset_mock()
+
+    # To mock `kubectl get svc` call's return with a loadBalancer ingress ip
+    run.return_value.stdout = yaml.safe_dump(
+        {"items": [{"status": {"loadBalancer": {"ingress": [{"ip": "127.0.0.1"}]}}}]}
+    ).encode("utf-8")
+    harness.framework.reemit()
 
     expected = [
         {
             'apiVersion': 'networking.istio.io/v1alpha3',
             'kind': 'VirtualService',
-            'metadata': {'name': 'service-name'},
+            'metadata': {
+                'name': 'service-name',
+                'labels': {'app.istio-pilot.io/is-workload-entity': 'true'},
+            },
             'spec': {
                 'gateways': ['None/istio-gateway'],
                 'hosts': ['*'],
@@ -89,17 +184,6 @@ def test_with_ingress_relation(harness, mocker):
                 ],
             },
         },
-        {
-            'apiVersion': 'networking.istio.io/v1beta1',
-            'kind': 'Gateway',
-            'metadata': {'name': 'istio-gateway'},
-            'spec': {
-                'selector': {'istio': 'ingressgateway'},
-                'servers': [
-                    {'hosts': ['*'], 'port': {'name': 'http', 'number': 80, 'protocol': 'HTTP'}}
-                ],
-            },
-        },
     ]
 
     assert check_call.call_args_list == [
@@ -116,39 +200,132 @@ def test_with_ingress_relation(harness, mocker):
         )
     ]
 
-    assert len(run.call_args_list) == 4
+    assert len(run.call_args_list) == 6
 
-    for call in run.call_args_list[::2]:
+    for call in run.call_args_list[1::3]:
         args = [
             './kubectl',
-            'delete',
-            'virtualservices,gateways',
-            '-lapp.juju.is/created-by=istio-pilot',
             '-n',
             None,
+            'delete',
+            'virtualservices,destinationrules',
+            '-lapp.juju.is/created-by=istio-pilot,app.istio-pilot.io/is-workload-entity=true',
         ]
         assert call.args == (args,)
 
-    for call in run.call_args_list[1::2]:
+    for call in run.call_args_list[2::3]:
         expected_input = list(yaml.safe_load_all(call.kwargs['input']))
-        assert call.args == (['./kubectl', 'apply', '-f-'],)
+        assert call.args == (['./kubectl', '-n', None, 'apply', '-f-'],)
         assert expected_input == expected
 
     assert isinstance(harness.charm.model.unit.status, ActiveStatus)
 
 
-def test_with_ingress_auth_relation(harness, mocker):
-    run = mocker.patch('subprocess.run')
-    check_call = mocker.patch('subprocess.check_call')
+def test_with_multiple_ingress_relations(harness, subprocess):
+    run = subprocess.run
+    check_call = subprocess.check_call
 
     harness.set_leader(True)
     harness.add_oci_resource(
         "noop",
+        {"registrypath": "", "username": "", "password": ""},
+    )
+
+    related_apps = ["app0", "app1"]
+    for app in related_apps:
+        rel_id = harness.add_relation("ingress", app)
+
+        harness.add_relation_unit(rel_id, f"{app}/0")
+        data = {"service": f"{app}-service-name", "port": 6666, "prefix": f"/{app}"}
+        harness.update_relation_data(
+            rel_id,
+            app,
+            {"_supported_versions": "- v1", "data": yaml.dump(data)},
+        )
+
+    run.return_value.stdout = b"{'items': []}"
+    harness.begin_with_initial_hooks()
+
+    assert run.call_count == 6
+    run.reset_mock()
+    run.return_value.stdout = yaml.safe_dump(
+        {"items": [{"status": {"loadBalancer": {"ingress": [{"ip": "127.0.0.1"}]}}}]}
+    ).encode("utf-8")
+    harness.framework.reemit()
+
+    expected = [
         {
-            "registrypath": "",
-            "username": "",
-            "password": "",
-        },
+            'apiVersion': 'networking.istio.io/v1alpha3',
+            'kind': 'VirtualService',
+            'metadata': {
+                'name': f'{app}-service-name',
+                'labels': {'app.istio-pilot.io/is-workload-entity': 'true'},
+            },
+            'spec': {
+                'gateways': ['None/istio-gateway'],
+                'hosts': ['*'],
+                'http': [
+                    {
+                        'match': [{'uri': {'prefix': f'/{app}'}}],
+                        'rewrite': {'uri': f'/{app}'},
+                        'route': [
+                            {
+                                'destination': {
+                                    'host': f'{app}-service-name.None.svc.cluster.local',
+                                    'port': {'number': 6666},
+                                }
+                            }
+                        ],
+                    }
+                ],
+            },
+        }
+        for app in related_apps
+    ]
+
+    assert check_call.call_args_list == [
+        Call(
+            [
+                './istioctl',
+                'install',
+                '-y',
+                '-s',
+                'profile=minimal',
+                '-s',
+                'values.global.istioNamespace=None',
+            ]
+        )
+    ]
+
+    assert len(run.call_args_list) == 12
+
+    for call in run.call_args_list[1::3]:
+        args = [
+            './kubectl',
+            '-n',
+            None,
+            'delete',
+            'virtualservices,destinationrules',
+            '-lapp.juju.is/created-by=istio-pilot,app.istio-pilot.io/is-workload-entity=true',
+        ]
+        assert call.args == (args,)
+
+    for call in run.call_args_list[2::3]:
+        expected_input = list(yaml.safe_load_all(call.kwargs['input']))
+        assert call.args == (['./kubectl', '-n', None, 'apply', '-f-'],)
+        assert expected_input == expected
+
+    assert isinstance(harness.charm.model.unit.status, ActiveStatus)
+
+
+def test_with_ingress_auth_relation(harness, subprocess):
+    run = subprocess.run
+    check_call = subprocess.check_call
+
+    harness.set_leader(True)
+    harness.add_oci_resource(
+        "noop",
+        {"registrypath": "", "username": "", "password": ""},
     )
     rel_id = harness.add_relation("ingress-auth", "app")
 
@@ -168,41 +345,55 @@ def test_with_ingress_auth_relation(harness, mocker):
 
     expected = [
         {
-            'apiVersion': 'rbac.istio.io/v1alpha1',
-            'kind': 'RbacConfig',
-            'metadata': {'name': 'default'},
-            'spec': {'mode': 'OFF'},
-        },
-        {
             'apiVersion': 'networking.istio.io/v1alpha3',
             'kind': 'EnvoyFilter',
-            'metadata': {'name': 'authn-filter'},
-            'spec': {
-                'filters': [
+            'metadata': {
+                'name': 'authn-filter',
+                'labels': {'app.istio-pilot.io/is-workload-entity': 'true'},
+            },
+            "spec": {
+                "configPatches": [
                     {
-                        'filterConfig': {
-                            'httpService': {
-                                'authorizationRequest': {
-                                    'allowedHeaders': {'patterns': [{'exact': 'foo'}]}
-                                },
-                                'authorizationResponse': {
-                                    'allowedUpstreamHeaders': {'patterns': [{'exact': 'bar'}]}
-                                },
-                                'serverUri': {
-                                    'cluster': 'outbound|6666||service-name.None.svc.cluster.local',
-                                    'failureModeAllow': False,
-                                    'timeout': '10s',
-                                    'uri': 'http://service-name.None.svc.cluster.local:6666',
-                                },
-                            }
+                        "applyTo": "HTTP_FILTER",
+                        "match": {
+                            "context": "GATEWAY",
+                            "listener": {
+                                "filterChain": {
+                                    "filter": {
+                                        "name": "envoy.filters.network.http_connection_manager"
+                                    }
+                                }
+                            },
                         },
-                        'filterName': 'envoy.ext_authz',
-                        'filterType': 'HTTP',
-                        'insertPosition': {'index': 'FIRST'},
-                        'listenerMatch': {'listenerType': 'GATEWAY'},
+                        "patch": {
+                            "operation": "INSERT_BEFORE",
+                            "value": {
+                                "name": "envoy.filters.http.ext_authz",
+                                "typed_config": {
+                                    "@type": "type.googleapis.com/envoy.extensions.filters.http."
+                                    "ext_authz.v3.ExtAuthz",
+                                    "http_service": {
+                                        'server_uri': {
+                                            'uri': 'http://service-name.None.svc.cluster.local:6666',  # noqa: E501
+                                            'cluster': 'outbound|6666||service-name.None.svc.'
+                                            'cluster.local',
+                                            'timeout': '10s',
+                                        },
+                                        "authorization_request": {
+                                            "allowed_headers": {'patterns': [{'exact': 'foo'}]}
+                                        },
+                                        "authorization_response": {
+                                            "allowed_upstream_headers": {
+                                                'patterns': [{'exact': 'bar'}]
+                                            }
+                                        },
+                                    },
+                                },
+                            },
+                        },
                     }
                 ],
-                'workloadLabels': {'istio': 'ingressgateway'},
+                "workloadSelector": {"labels": {"istio": "ingressgateway"}},
             },
         },
     ]
@@ -221,22 +412,22 @@ def test_with_ingress_auth_relation(harness, mocker):
         )
     ]
 
-    assert len(run.call_args_list) == 4
+    assert len(run.call_args_list) == 6
 
-    for call in run.call_args_list[::2]:
+    for call in run.call_args_list[2::2]:
         args = [
             './kubectl',
-            'delete',
-            'envoyfilters,rbacconfigs',
-            '-lapp.juju.is/created-by=istio-pilot',
             '-n',
             None,
+            'delete',
+            'envoyfilters',
+            '-lapp.juju.is/created-by=istio-pilot,app.istio-pilot.io/is-workload-entity=true',
         ]
         assert call.args == (args,)
 
-    for call in run.call_args_list[1::2]:
+    for call in run.call_args_list[5::2]:
         expected_input = list(yaml.safe_load_all(call.kwargs['input']))
-        assert call.args == (['./kubectl', 'apply', '-f-'],)
+        assert call.args == (['./kubectl', '-n', None, 'apply', '-f-'],)
         assert expected_input == expected
 
     assert isinstance(harness.charm.model.unit.status, ActiveStatus)
